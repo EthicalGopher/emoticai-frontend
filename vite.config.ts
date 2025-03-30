f4c6ecf51fa7ccfa3bee2903360b42744ccd3e60
Sankhyahrick Swami <sankhyahricks@gmail.com>
2025-03-30 09:36:32 +0530

    3rd

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,14 @@
-
 import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react-swc"
 import path from "path"
 import { componentTagger } from "lovable-tagger"
 
-export default defineConfig(({ mode }: { mode: string }) => ({
+export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
     port: 8080,
-    allowedHosts: "all"
   },
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+  plugins: [react(), mode === "development" ? componentTagger() : null].filter(Boolean),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
